8536d3f3db5bf46a680ec94061eae2d3c8cce106
85ar <85ar@rambler.ru>
2021-07-05 16:34:22 +0500

    fix: footer

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 const swiper = new Swiper(".swiper-container", {
-  // Optional parameters
   direction: "horizontal",
   loop: true,
-
+  slidesPerView: 1,
   keyboard: {
     enabled: true,
   },
-
   pagination: {
     el: ".swiper-pagination",
   },
-
   navigation: {
     nextEl: ".swiper-button-next",
+  },
+});
+const swiper2 = new Swiper(".partners-wrapper", {
+  direction: "horizontal",
+  spaceBetween: 50,
+  slidesPerView: 7,
+  loop: true,
+  // autoplay: {
+  //   delay: 1200,
+  // },
+  keyboard: {
+    enabled: true,
   },
 });
