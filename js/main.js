ad73f2866f10089367ba73f4969f4027d0183f91
85ar <85ar@rambler.ru>
2021-07-06 01:02:33 +0500

    fix: modal

--- js/main.js
+++ js/main.js
@@ -24,3 +24,48 @@ const swiper2 = new Swiper(".partners-wrapper", {
     enabled: true,
   },
 });
+AOS.init();
+
+AOS.init({
+  disable: function () {
+    var maxWidth = 992;
+    return window.innerWidth < maxWidth;
+  },
+});
+
+var modalButton = $("[data-toggle=modal]");
+var closeModalButton = $(".modal__close");
+modalButton.on("click", openModal);
+closeModalButton.on("click", closeModal);
+
+function openModal() {
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.addClass("modal__overlay--visible");
+  modalDialog.addClass("modal__dialog--visible");
+}
+function closeModal(event) {
+  event.preventDefault();
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass("modal__overlay--visible");
+  modalDialog.removeClass("modal__dialog--visible");
+}
+$(document).on("keyup", function (esc) {
+  if (esc.keyCode == 27) {
+    esc.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+});
+$(document).mouseup(function (e) {
+  var container = $(".modal__dialog");
+  if (container.has(e.target).length === 0) {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+});
